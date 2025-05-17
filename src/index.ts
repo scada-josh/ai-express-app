bcf0cb17fd101f4d3f82ab092da67bea4baea77c
“Anusorn <scada.josh@gmail.com>
2025-05-17 12:24:34 +0700

    first commit, hello world

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, {Request, Response} from "express";
+
+const app = express();
+
+app.use("/", (_req: Request, res: Response) => {
+    res.json({message: "Hello World"});
+})
+
+app.listen(4000, ()=> {
+    console.log("Server runnig on port 4000");
+});
